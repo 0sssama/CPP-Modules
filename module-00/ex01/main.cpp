e92997a409a2809ba6af865173c75d469d3423c1
Oussama <0ssama@protonmail.com>
2022-07-10 22:05:46 +0100

    :gear: init stdinput

--- module-00/ex01/main.cpp
+++ module-00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: olabrahm <olabrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 14:12:58 by olabrahm          #+#    #+#             */
-/*   Updated: 2022/07/05 14:13:17 by olabrahm         ###   ########.fr       */
+/*   Updated: 2022/07/10 22:03:21 by olabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,14 @@
 int	main(void)
 {
 	PhoneBook	phonebook;
+	std::string	input;
 	
+	while (69)
+	{
+		std::cout << "CMD >> ";
+		if (!getline(std::cin, input))
+			return ((std::cout << std::endl), 0);
+		std::cout << "You typed : " << input << std::endl;	
+	}
 	return (0);
 }
