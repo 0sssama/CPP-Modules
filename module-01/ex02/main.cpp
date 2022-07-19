f7e7d23ebb32f0f987925305da12cbf11292f9d7
Oussama Labrahmi <olabrahm@e3r11p13.1337.ma>
2022-07-19 07:56:01 +0100

    :gear: finished ex02

--- module-01/ex02/main.cpp
+++ module-01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: olabrahm <olabrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 07:39:35 by olabrahm          #+#    #+#             */
-/*   Updated: 2022/07/19 07:40:42 by olabrahm         ###   ########.fr       */
+/*   Updated: 2022/07/19 07:46:59 by olabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,18 @@
 
 int	main(void)
 {
-	std::cout << "hello world" << std::endl;
+	std::string		string = "HI THIS IS BRAIN";
+	std::string		*stringPTR = &string;
+	std::string		&stringREF = string;
+
+	std::cout << "string address: " << &string << std::endl;
+	std::cout << "stringPTR address: " << &stringPTR << std::endl;
+	std::cout << "stringREF address: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "string value: " << string << std::endl;
+	std::cout << "stringPTR points to: " << *stringPTR << std::endl;
+	std::cout << "stringREF address: " << stringREF << std::endl;
 	return 0;
 }
