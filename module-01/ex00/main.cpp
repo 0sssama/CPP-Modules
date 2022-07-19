7fb782f7bd528a124555ca62c23d2fe3897e5d2a
Oussama Labrahmi <olabrahm@e3r11p13.1337.ma>
2022-07-19 07:11:23 +0100

    :gear: few edits to main

--- module-01/ex00/main.cpp
+++ module-01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: olabrahm <olabrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 14:38:48 by olabrahm          #+#    #+#             */
-/*   Updated: 2022/07/18 14:59:14 by olabrahm         ###   ########.fr       */
+/*   Updated: 2022/07/19 07:10:52 by olabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(void)
 {
-	Zombie	*hmed = newZombie("Hmed");
+	Zombie	*hmed = new Zombie("Hmed");
 
 	hmed->announce();
 	return 0;
