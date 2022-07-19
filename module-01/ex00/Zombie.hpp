2b773696a0b0cbcd507ed7e3c802b8de473f0017
Oussama Labrahmi <olabrahm@e3r11p13.1337.ma>
2022-07-19 07:38:23 +0100

    :gear: few edits to zombie class

--- module-01/ex00/Zombie.hpp
+++ module-01/ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: olabrahm <olabrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 14:40:06 by olabrahm          #+#    #+#             */
-/*   Updated: 2022/07/19 07:08:24 by olabrahm         ###   ########.fr       */
+/*   Updated: 2022/07/19 07:28:54 by olabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,19 @@ class Zombie
 {
 
 	private:
-		std::string				name;
+		std::string	name;
 	
 	public:
+	
 		Zombie(std::string name);
+		Zombie(void);
 		~Zombie(void);
-		void					announce(void);
-		std::string				getName(void);
+
+
+		void		announce(void);
+
+		std::string	getName(void);
+		void		setName(std::string name);
 
 };
 
