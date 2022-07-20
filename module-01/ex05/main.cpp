1f1a9f0989a3d6b2922de1637d2f14be6dc34c60
Oussama Labrahmi <olabrahm@e3r11p8.1337.ma>
2022-07-20 09:52:52 +0100

    :gear: finished day 01

--- module-01/ex05/main.cpp
+++ module-01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: olabrahm <olabrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 07:51:59 by olabrahm          #+#    #+#             */
-/*   Updated: 2022/07/20 09:15:19 by olabrahm         ###   ########.fr       */
+/*   Updated: 2022/07/20 09:52:32 by olabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 
 int	main(void)
 {
-	Harl	harl;
+	Harl	*harl = new Harl();
 
-	harl.complain("DEBUG");
-	harl.complain("INFO");
-	harl.complain("WARNING");
-	harl.complain("ERROR");
+	harl->complain("DEBUG");
+
+	delete harl;
 	return (0);
 }
