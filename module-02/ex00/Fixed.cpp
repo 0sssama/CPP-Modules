0b6e0cc1245771f3a6ce739c573ad5bd12b5982a
Oussama Labrahmi <0ssama@protonmail.com>
2022-11-17 11:34:09 +0100

    fixing ex00

--- module-02/ex00/Fixed.cpp
+++ module-02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: olabrahm <olabrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 20:05:27 by olabrahm          #+#    #+#             */
-/*   Updated: 2022/10/29 20:31:20 by olabrahm         ###   ########.fr       */
+/*   Updated: 2022/11/17 11:33:18 by olabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,18 @@ Fixed::~Fixed(void)
 Fixed::Fixed(const Fixed &fixed)
 {
 	std::cout << "Copy constructor called" << std::endl;
-	this->_value = fixed._value;
+	this->_value = fixed.getRawBits();
 }
 
 Fixed	&Fixed::operator=(const Fixed &fixed)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
-	this->_value = fixed._value;
+	this->_value = fixed.getRawBits();
 	return (*this);
 }
 
 int	Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
 	return (this->_value);
 }
 
--- module-02/ex00/main.cpp
+++ module-02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: olabrahm <olabrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 20:05:13 by olabrahm          #+#    #+#             */
-/*   Updated: 2022/10/29 20:32:53 by olabrahm         ###   ########.fr       */
+/*   Updated: 2022/11/17 11:32:21 by olabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int	main(void)
 	Fixed	c;
 
 	c = b;
-	b.setRawBits(10);
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
